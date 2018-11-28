1c6ad24d62acf00dac2ce7874e6f161b17b244a2
thor1824 <42994659+thor1824@users.noreply.github.com>
2018-11-29 00:54:13 +0100

    Gui Controller
    jeg har
    - gjort så den kan afspille mp3 filer
    - syncronisere timer og bar med med sang
    - formateret Timeren

--- src/mytunes/GUI/FXMLController.java
+++ src/mytunes/GUI/FXMLController.java
@@ -23,23 +23,35 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import static java.lang.Math.floor;
+import static java.lang.String.format;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TableColumn;
+import javafx.scene.input.MouseButton;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import mytunes.BE.Song;
+import javafx.beans.Observable;
 
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
@@ -80,6 +92,8 @@ public class FXMLController implements Initializable {
     private Label lbltime;
     @FXML
     private Button btnEdit;
+    @FXML
+    private ProgressBar songProg;
 
     //other
     private MediaPlayer mPlayer;
@@ -87,6 +101,10 @@ public class FXMLController implements Initializable {
     private int paused = 1;
     private List<Song> activePlaylist;
     private Song activeSong;
+    private javafx.beans.value.ChangeListener<Duration> progressChangeListener;
+    @FXML
+    private MediaView mview;
+    private Duration duration;
 
     /**
      * Initializes the controller class.
@@ -95,13 +113,15 @@ public class FXMLController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         try {
             mtModel = new MyTunesModel();
-
-//            activePlaylist = mtModel.getAllSong();
-//            tbvSongs.getItems().addAll(activePlaylist);
+            String path = new File("src/Music/Dee_Yan-Key_-_01_-_That_aint_Chopin.mp3").getAbsolutePath();
+            path.replace("\\", "/").replaceAll(" ", "%20");
+            Media media = new Media(new File(path).toURI().toString());
+            mPlayer = new MediaPlayer(media);
         } catch (SQLException ex) {
             Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
         }
 
+        //Collum Init
         TableColumn<Song, String> title = new TableColumn<>();
         title.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getTitle()));
         title.setText("Title");
@@ -114,9 +134,8 @@ public class FXMLController implements Initializable {
         artist.setPrefWidth(200);
         tbvSongs.getColumns().add(artist);
 
-        //Husk denne skal ændres fra getTitle til genrer i stedet for
         TableColumn<Song, String> genrer = new TableColumn<>();
-        genrer.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getTitle()));
+        genrer.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().getGenre()));
         genrer.setText("Genre");
         genrer.setPrefWidth(150);
         tbvSongs.getColumns().add(genrer);
@@ -127,13 +146,39 @@ public class FXMLController implements Initializable {
         durations.setPrefWidth(80);
         tbvSongs.getColumns().add(durations);
 
+        //Event init
+        tbvSongs.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
+                    if (mouseEvent.getClickCount() == 2) {
+                        Song song = tbvSongs.getSelectionModel().getSelectedItem();
+                        playSong(song);
+                    }
+                }
+            }
+        });
+
+        //timer
+        updateTimer();
+
+        //slider
+        updateSlide();
+
+        //other init
+//        activeSong = activePlaylist.get(0);
+//        activePlaylist = mtModel.getAllSong();
+//        tbvSongs.getItems().addAll(activePlaylist);
     }
 
     @FXML
     private void volSlider(MouseEvent event) {
-        double volume = sldVol.getBaselineOffset();
-        double max = sldVol.getMax();
-        mPlayer.setVolume((volume / max * 100) / 100);
+        if (mPlayer != null) {
+            double volume = sldVol.getValue();
+            double max = sldVol.getMax();
+            mPlayer.setVolume((volume / max * 100) / 100);
+        }
+
     }
 
     @FXML
@@ -154,26 +199,41 @@ public class FXMLController implements Initializable {
     private void playSongBtn(MouseEvent event) {
         Image pause = new Image("Pics/PauseBtn.png");
         Image play = new Image("Pics/Playbtn.png");
+
         switch (paused) {
             case 0:
                 btnPlay.setImage(play);
+                mPlayer.pause();
                 paused = 1;
                 break;
             case 1:
 
                 btnPlay.setImage(pause);
+                playSong(null);
                 paused = 0;
                 break;
+
         }
     }
 
     @FXML
     private void NextSongBtn(MouseEvent event) {
-
+        int nextIndex = activePlaylist.indexOf(activeSong) + 1;
+        if ((activePlaylist.size() - 1) < nextIndex) {
+            nextIndex = 0;
+        }
+        activeSong = activePlaylist.get(nextIndex);
+        playSong(activeSong);
     }
 
     @FXML
     private void prevSongBtn(MouseEvent event) {
+        int previousIndex = activePlaylist.indexOf(activeSong) - 1;
+        if (previousIndex > 0) {
+            previousIndex = activePlaylist.size()-1;
+        }
+        activeSong = activePlaylist.get(previousIndex);
+        playSong(activeSong);
     }
 
     @FXML
@@ -221,6 +281,57 @@ public class FXMLController implements Initializable {
         }
     }
 
+    private void updateSlide() {
+        sldProg.valueProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated(Observable observable) {
+                if (sldProg.isValueChanging()) {
+                    // multiply duration by percentage calculated by slider position
+                    if (duration != null) {
+                        mPlayer.seek(duration.multiply(sldProg.getValue() / 100.0));
+                    }
+                    updateValues();
+
+                }
+            }
+        });
+
+        mPlayer.currentTimeProperty().addListener(new ChangeListener() {
+
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                updateValues();
+            }
+        });
+    }
+
+    private void updateTimer() {
+        mPlayer.currentTimeProperty().addListener((Observable ov) -> {
+            updateValues();
+        });
+
+        mPlayer.setOnReady(() -> {
+            duration = mPlayer.getMedia().getDuration();
+            updateValues();
+        });
+    }
+
+    private void updateValues() {
+        if (lbltime != null && sldProg != null && duration != null) {
+            Platform.runLater(new Runnable() {
+                public void run() {
+                    Duration currentTime = mPlayer.getCurrentTime();
+                    lbltime.setText(formatTime(currentTime, duration));
+                    sldProg.setDisable(duration.isUnknown());
+                    if (!sldProg.isDisabled() && duration.greaterThan(Duration.ZERO) && !sldProg.isValueChanging()) {
+                        sldProg.setValue(currentTime.divide(duration).toMillis() * 100.0);
+                    }
+
+                }
+            });
+        }
+    }
+
     private void createSongFromMetadata(File addedFile) throws IOException, SAXException, SQLException, FileNotFoundException, TikaException, NumberFormatException {
         InputStream input = new FileInputStream(addedFile);
         ContentHandler handler = new DefaultHandler();
@@ -240,9 +351,67 @@ public class FXMLController implements Initializable {
     }
 
     private void playSong(Song song) {
-        Media media = new Media(song.getFilePath());
+        mPlayer.stop();
+//        String path = new File(song.getFilePath()).getAbsolutePath();
+        String path = new File("src/Music/Dee_Yan-Key_-_01_-_That_aint_Chopin.mp3").getAbsolutePath();
+        path.replace("\\", "/").replaceAll(" ", "%20");
+        Media media = new Media(new File(path).toURI().toString());
         mPlayer = new MediaPlayer(media);
-        mPlayer.play();
+        updateValues();
+        mview = new MediaView(mPlayer);
+
+        mPlayer.setOnReady(new Runnable() {
+            @Override
+            public void run() {
+                updateSlide();
+                updateTimer();
+                mview.setMediaPlayer(mPlayer);
+                mPlayer.play();
+            }
+        });
+
+        //lblPlaying.setText(song.getTitle());
+//        volSlider(null);
+//        System.out.println(mPlayer.getTotalDuration());
+    }
+
+    private static String formatTime(Duration elapsed, Duration duration) {
+        int intElapsed = (int) floor(elapsed.toSeconds());
+        int elapsedHours = intElapsed / (60 * 60);
+        if (elapsedHours > 0) {
+            intElapsed -= elapsedHours * 60 * 60;
+        }
+        int elapsedMinutes = intElapsed / 60;
+        int elapsedSeconds = intElapsed - elapsedHours * 60 * 60
+                - elapsedMinutes * 60;
+
+        if (duration.greaterThan(Duration.ZERO)) {
+            int intDuration = (int) floor(duration.toSeconds());
+            int durationHours = intDuration / (60 * 60);
+            if (durationHours > 0) {
+                intDuration -= durationHours * 60 * 60;
+            }
+            int durationMinutes = intDuration / 60;
+            int durationSeconds = intDuration - durationHours * 60 * 60
+                    - durationMinutes * 60;
+            if (durationHours > 0) {
+                return format("%d:%02d:%02d/%d:%02d:%02d",
+                        elapsedHours, elapsedMinutes, elapsedSeconds,
+                        durationHours, durationMinutes, durationSeconds);
+            } else {
+                return format("%02d:%02d/%02d:%02d",
+                        elapsedMinutes, elapsedSeconds, durationMinutes,
+                        durationSeconds);
+            }
+        } else {
+            if (elapsedHours > 0) {
+                return format("%d:%02d:%02d", elapsedHours,
+                        elapsedMinutes, elapsedSeconds);
+            } else {
+                return format("%02d:%02d", elapsedMinutes,
+                        elapsedSeconds);
+            }
+        }
     }
 
     @FXML
